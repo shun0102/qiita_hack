f1bca077f7f288b67acc329f415d9501b4b537b3
Shunsuke Mikami <shun0102@gmail.com>
2012-10-13 11:02:12 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QiitaHack::Application.config.secret_token = '45fe99ce7f99f21268f5e6f4a0fd9bf5bc144f61120be77497979f0cf984ca9ffd426b228da1f3db80778e739507f472f32a8e7c8f555ae2d0fd481d675586a3'
